d6a7a13e41fbdfdf6d77bd29f67023b0f7d1bb48
twotolab <patrick@twoto.com>
2016-02-11 10:18:15 +0100

    update

---
+++ html_Files/actual/final/assets/js/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($) {
+	//	var isLateralNavAnimating = false;
+	//open/close lateral navigation
+	$('.menuHandler').on('click', function(event) {
+		event.preventDefault();
+		if( $('.nav').hasClass('is-visible') ) {
+			$('.nav').removeClass('is-visible');
+			$('.navi-close').removeClass('is-visible');
+			$('.navi-menu').removeClass('is-invisible');
+			$('.navi-logo').removeClass('is-visible');
+			$('.MenuWrapper').removeClass('menu-out');
+		} else{
+			$('.nav').addClass('is-visible');
+			$('.navi-menu').addClass('is-invisible');
+			$('.navi-close').addClass('is-visible');
+			$('.navi-logo').addClass('is-visible');
+			$('.MenuWrapper').addClass('menu-out');
+		}
+	});
+});
