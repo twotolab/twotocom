2ef1485cda85ae3c68cca2278464752203743c2e
twotolab <patrick@twoto.com>
2016-02-23 10:46:22 +0100

    update. header .margin .sidenavi

--- html_Files/actual/final/assets/js/main.js
+++ html_Files/actual/final/assets/js/main.js
@@ -1,28 +1,3 @@
-/*jQuery(document).ready(function($) {
-	//	var isLateralNavAnimating = false;
-	//open/close lateral navigation
-	$('.menu-button').on('click', function(event) {
-		event.preventDefault();
-		if( $('body').hasClass('nav-is-visible') ) {
-			$('body').removeClass('nav-is-visible');
-			$('.nav-main').removeClass('is-visible');
-			$('.menu-close').removeClass('is-visible');
-			$('.menu-open').removeClass('is-invisible');
-			$('.nav-quote-bg').removeClass('is-visible');
-			$('.nav-bg-quote').removeClass('is-visible');
-			$('.menu-wrapper').removeClass('menu-out');
-		} else{
-			$('body').addClass('nav-is-visible');
-			$('.nav-main').addClass('is-visible');
-			$('.menu-open').addClass('is-invisible');
-			$('.menu-close').addClass('is-visible');
-			$('.nav-quote-bg').addClass('is-visible');
-			$('.nav-bg-quote').addClass('is-visible');
-			$('.menu-wrapper').addClass('menu-out');
-		}
-	});
-});
-*/
 jQuery(document).ready(function($) {
 	//	var isLateralNavAnimating = false;
 	//open/close lateral navigation
@@ -50,6 +25,8 @@ jQuery(document).ready(function($) {
 			$('.nav-bg-quote').removeClass('is-visible');
 			$('.menu-wrapper').removeClass('menu-out');
 		} else{
+			//scrollTop(0);
+			$('body').animate({scrollTop:0}, '500');
 			$('body').addClass('nav-is-visible');
 			$('.nav-main').addClass('is-visible');
 			$('.menu-open').addClass('is-invisible');
