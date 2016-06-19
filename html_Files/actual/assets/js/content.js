cc3d89a2d0be463c1632d5b19bed144574edd745
patrick Decaix <patrick.decaix@xing.com>
2016-06-19 02:20:35 +0200

    update header background

--- html_Files/actual/assets/js/content.js
+++ html_Files/actual/assets/js/content.js
@@ -57,8 +57,9 @@ jQuery(document).ready(function($) {
 		posYHeader=  parseInt($('.header-bg').css('top'),10) || 0 ;
 		if(currentPoxY > changeHeader){
 			if(headerStatusOpen){
-				$('.header-bg').css({"transition": "all 0.3s ease-out"});	
-				$('.header-bg').css({"transform": "translate3d( 0px, " + -headerContainerHeight + "px, 0px)"});	
+				$('.header-bg').css({"transition": "all 1.6s ease-out"});	
+				//$('.header-bg').css({"transform": "translate3d( 0px, " + -headerContainerHeight + "px, 0px)"});	
+				$('.header-bg').css({"transform": "translate3d(0px, -200px,0px)"});	
 				//console.log('close it--------'+currentPoxY);
 			}
 			headerStatusOpen = false;
@@ -66,7 +67,7 @@ jQuery(document).ready(function($) {
 		}
 		if(currentPoxY <= changeHeader){
 			if(!headerStatusOpen){
-				$('.header-bg').css({"transition": "all 0.6s ease-out"});
+				$('.header-bg').css({"transition": "all 1.6s ease-out"});
 				$('.header-bg').css({"transform": "translate3d(0px,0px, 0px)"});	
 				console.log('open it--------'+currentPoxY);	
 			}
@@ -74,7 +75,7 @@ jQuery(document).ready(function($) {
 			
 		}
 	}
-	var added = 104;
+	var added = 205;//104;
 	function updateHeaderHeight(){
 		headerContainerHeight = $('.header-inside').height()+ added;
 		$('.header-bg').css({height:headerContainerHeight});
